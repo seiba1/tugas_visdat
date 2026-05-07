ffc4f3b225a001dc474086f40cd687bbf7c1a8af
Mokh. Faisol Nahdah Maulana <faisalmaulana241@gmail.com>
2026-05-07 22:22:43 +0700

    Update apps.py

--- apps.py
+++ apps.py
@@ -13,6 +13,7 @@ st.set_page_config(page_title="Analisis Penjualan Southwind", layout="wide")
 @st.cache_data
 def load_data():
     # Membaca file CSV yang diunggah
+    url = 'https://github.com/seiba1/tugas_visdat/blob/main/Southwind.csv'
     df = pd.read_csv('Southwind.csv')
     # Membersihkan kolom (opsional, menyesuaikan dengan data Southwind)
     df = df[['Penjualan', 'jumlah', 'diskon', 'keuntungan']].dropna()
